fd72a7a9809310e61f53c08b08130e7be00c2e55
Aziza33 <aitlahcen.aziza@gmail.com>
2025-09-24 10:40:58 +0200

    Update commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Components, useReducer } from 'react'
+import Count from './components/Count'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Test</h1>
+        <Count />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
